5a8fbf5af2e211eec452358a1c3caad016681343
livelycloud <qywangyj@gmail.com>
2022-03-21 15:41:05 +0800

    update NetManager

--- testing_app/lib/request/NetManager.dart
+++ testing_app/lib/request/NetManager.dart
@@ -69,16 +69,14 @@ class NetManager {
   /// [year], 'int'.
   /// [month], 'int'.
   /// This function returns [ReportModel] with [ReportData] as its data.
-  Future<ReportModel> makeGetMonthReportModelRequest(
-      Map<String, String> curHeaders, int year, int month) async {
+  Future<ReportModel> makeGetMonthReportModelRequest(int year, int month) async {
     return makeGetMonthReportModelRequest_(config, year, month);
   }
 
   /// make get year report request to the server/
   /// [year], 'int'.
   /// This function returns [ReportModel] with [ReportData] as its data.
-  Future<ReportModel> makeGetYearReportModelRequest(
-      Map<String, String> curHeaders, int year) async {
+  Future<ReportModel> makeGetYearReportModelRequest(int year) async {
     return makeGetYearReportModelRequest_(config, year);
   }
 
