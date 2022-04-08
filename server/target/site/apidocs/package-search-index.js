31930479e0e21c5a69727d8dacd09981d76dd586
maffinnn <70844093+maffinnn@users.noreply.github.com>
2022-04-08 12:11:09 +0800

    Add files via upload

---
+++ server/target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.cz2006.group3"},{"l":"com.cz2006.group3.bean"},{"l":"com.cz2006.group3.servlet"}];updateSearchResults();
