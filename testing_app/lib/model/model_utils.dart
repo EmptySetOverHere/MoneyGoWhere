bf61d7d7225ff4af82b7859722fc171740645a6f
maffinnn <70844093+maffinnn@users.noreply.github.com>
2022-04-14 21:11:15 +0800

    Delete testing_app directory

--- testing_app/lib/model/model_utils.dart
+++
@@ -1,7 +0,0 @@
-///This file is used to export all dart files under model
-
-export 'MerchantsModel.dart';
-export 'ReceiptModel.dart' hide main;
-export 'ReportModel.dart' hide main;
-export 'UserModel.dart' hide main;
-export  'MessageModel.dart';
--- testing_app/lib/request/request_utils.dart
+++
@@ -1,8 +0,0 @@
-library request_utils;
-
-///This file is used to export all dart files under request
-export 'ReceiptsRequest.dart';
-export 'ReportsRequest.dart';
-export 'MerchantsRequest.dart';
-export 'UserRequest.dart';
-export 'AccountRequest.dart';
